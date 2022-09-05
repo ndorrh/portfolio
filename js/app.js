787437687656600af08cc0f8ee7c2f73ba865656
ndorrh <ndorrh@gmail.com>
2022-09-06 00:30:25 +0100

    fix linter errors

--- js/app.js
+++ js/app.js
@@ -3,31 +3,29 @@ const closeBtn = document.querySelector('.closed');
 const navList = document.querySelector('.navlist');
 const logo = document.querySelector('.logo');
 
-hamburger.addEventListener('click', function (){
-    if(navList.style.display = 'none'){
-        navList.style.display = 'block'
-        closeBtn.style.display = 'block'
-        hamburger.style.display = "none" 
-        logo.style.display = "none" 
-    }
-})
+hamburger.addEventListener('click', () => {
+  if (navList.style.display === 'none') {
+    navList.style.display = 'block';
+    closeBtn.style.display = 'block';
+    hamburger.style.display = 'none';
+    logo.style.display = 'none';
+  }
+});
 
-closeBtn.addEventListener('click', function (){
-    if(navList.style.display = 'block'){
-        navList.style.display = 'none'
-        closeBtn.style.display = 'none'
-        hamburger.style.display = "block" 
-        logo.style.display = "block" 
-    }
-})
+closeBtn.addEventListener('click', () => {
+  if (navList.style.display === 'block') {
+    navList.style.display = 'none';
+    closeBtn.style.display = 'none';
+    hamburger.style.display = 'block';
+    logo.style.display = 'block';
+  }
+});
 
-navList.addEventListener('click', function () {
-    if (navList.style.display = 'block') {
-        navList.style.display = 'none'
-        closeBtn.style.display = 'none'
-        hamburger.style.display = "block"
-        logo.style.display = "block"
-    }
-})
-
-
+navList.addEventListener('click', () => {
+  if (navList.style.display === 'block') {
+    navList.style.display = 'none';
+    closeBtn.style.display = 'none';
+    hamburger.style.display = 'block';
+    logo.style.display = 'block';
+  }
+});
