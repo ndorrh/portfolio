ec8f58d47e0c1bf596c9dfa8845e4920334c61d5
ndorrh <ndorrh@gmail.com>
2022-09-06 07:26:37 +0100

    Update app.js to remove redundacy and stop the hamburger icon from appearing on desktop when menu item is clicked

--- js/app.js
+++ js/app.js
@@ -3,27 +3,28 @@ const hamburger = document.querySelector('.hamburger-menu');
 const closeBtn = document.querySelector('.closed');
 const navList = document.querySelector('.navlist');
 const logo = document.querySelector('.logo');
+const widerScreenWidth = window.matchMedia('(max-width: 768px)');
 
-hamburger.addEventListener('click', () => {
+const menuOpen = () => {
   navList.style.display = 'none';
   navList.style.display = 'block';
   closeBtn.style.display = 'block';
   hamburger.style.display = 'none';
   logo.style.display = 'none';
-});
+};
 
-closeBtn.addEventListener('click', () => {
-  navList.style.display = 'block';
-  navList.style.display = 'none';
-  closeBtn.style.display = 'none';
-  hamburger.style.display = 'block';
-  logo.style.display = 'block';
-});
+const menuClose = () => {
+  if (widerScreenWidth.matches) {
+    navList.style.display = 'block';
+    navList.style.display = 'none';
+    closeBtn.style.display = 'none';
+    hamburger.style.display = 'block';
+    logo.style.display = 'block';
+  }
+};
 
-navList.addEventListener('click', () => {
-  navList.style.display = 'block';
-  navList.style.display = 'none';
-  closeBtn.style.display = 'none';
-  hamburger.style.display = 'block';
-  logo.style.display = 'block';
-});
+hamburger.addEventListener('click', menuOpen);
+
+closeBtn.addEventListener('click', menuClose);
+
+navList.addEventListener('click', menuClose);
